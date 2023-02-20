2cc418d3a0cabfe52a3f02406c54f9c680e174de
Nathan BENOIT <nathan.benoit@epsi.fr>
2023-02-20 10:28:29 +0100

    ajout d'un nouveau projet de test

--- TestAPIRevendeurs/TestProduitsController.cs
+++ TestAPIRevendeurs/TestProduitsController.cs
@@ -1,11 +1,42 @@
 ﻿using System;
-namespace TestAPIRevendeurs
-{
-	public class TestProduitsController
-	{
-		public TestProduitsController()
-		{
-		}
-	}
-}
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+using APIRevendeurs;
+using APIRevendeurs.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+namespace StoreApp.Tests
+{
+    [TestClass]
+    public class TestSimpleProductController
+    {
+        [TestMethod]
+        public void GetAllProducts_ShouldReturnAllProducts()
+        {
+            var controller = new ProduitsController();
+
+            var result = controller.GetAsync() as Task<List<Produits>>;
+            Assert.AreEqual(testProducts.Count, result.Count);
+        }
+
+        [TestMethod]
+        public void GetProduct_ShouldReturnCorrectProduct()
+        {
+            var controller = new ProduitsController();
+
+            var result = controller.Get(4) as Task<Produits>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(testProducts[3].Name, result.Content.Name);
+        }
+
+        [TestMethod]
+        public void GetProduct_ShouldNotFindProduct()
+        {
+            var controller = new ProduitsController();
+
+            var result = controller.Get(999);
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+        }
+    }
+}
